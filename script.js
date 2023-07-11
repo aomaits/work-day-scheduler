674405748ac5ea293c69079ad0f6e197ec0ec00d
Abe Omaits <aomaits@gmail.com>
2023-07-11 17:53:41 -0500

    replicated event storage and withdrawal code blocks throughout day

--- script.js
+++ script.js
@@ -2,9 +2,25 @@ var dateToday = dayjs().format('dddd, MMMM D YYYY');
 var currentHour =dayjs().format('HH');
 var numCurrentHour = Number(currentHour);
 
-
 var hour09event = $(body).children().eq(1).children().eq(0).children().eq(1);
+var hour10event = $(body).children().eq(1).children().eq(1).children().eq(1);
+var hour11event = $(body).children().eq(1).children().eq(2).children().eq(1);
+var hour12event = $(body).children().eq(1).children().eq(3).children().eq(1);
+var hour13event = $(body).children().eq(1).children().eq(4).children().eq(1);
+var hour14event = $(body).children().eq(1).children().eq(5).children().eq(1);
+var hour15event = $(body).children().eq(1).children().eq(6).children().eq(1);
+var hour16event = $(body).children().eq(1).children().eq(7).children().eq(1);
+var hour17event = $(body).children().eq(1).children().eq(8).children().eq(1);
+
 var hour09button = $(body).children().eq(1).children().eq(0).children().eq(2);
+var hour10button = $(body).children().eq(1).children().eq(1).children().eq(2);
+var hour11button = $(body).children().eq(1).children().eq(2).children().eq(2);
+var hour12button = $(body).children().eq(1).children().eq(3).children().eq(2);
+var hour13button = $(body).children().eq(1).children().eq(4).children().eq(2);
+var hour14button = $(body).children().eq(1).children().eq(5).children().eq(2);
+var hour15button = $(body).children().eq(1).children().eq(6).children().eq(2);
+var hour16button = $(body).children().eq(1).children().eq(7).children().eq(2);
+var hour17button = $(body).children().eq(1).children().eq(8).children().eq(2);
 
 var hour09 = $('#hour-09');
 var hour10 = $('#hour-10');
@@ -16,104 +32,6 @@ var hour15 = $('#hour-15');
 var hour16 = $('#hour-16');
 var hour17 = $('#hour-17');
 
-pastPresFut_hour09();
-pastPresFut_hour10();
-pastPresFut_hour11();
-pastPresFut_hour12();
-pastPresFut_hour13();
-pastPresFut_hour14();
-pastPresFut_hour15();
-pastPresFut_hour16();
-pastPresFut_hour17();
-
-
-function pastPresFut_hour09() {
-    if (numCurrentHour < 9) {
-        hour09.addClass('future');
-    } else if (numCurrentHour === 9) {
-        hour09.addClass('present');
-    } else {
-        hour09.addClass('past'); 
-    }
-}
-
-function pastPresFut_hour10() {
-        if (numCurrentHour < 10) {
-            hour10.addClass('future');
-        } else if (numCurrentHour === 10) {
-            hour10.addClass('present');
-        } else {
-            hour10.addClass('past'); 
-        }
-}
-function pastPresFut_hour11() {
-        if (numCurrentHour < 11) {
-            hour11.addClass('future');
-        } else if (numCurrentHour === 11) {
-            hour11.addClass('present');
-        } else {
-            hour11.addClass('past'); 
-        }
-}
-function pastPresFut_hour12() {
-    if (numCurrentHour < 12) {
-        hour12.addClass('future');
-    } else if (numCurrentHour === 12) {
-        hour12.addClass('present');
-    } else {
-        hour12.addClass('past'); 
-    }
-}
-function pastPresFut_hour13() {
-    if (numCurrentHour < 13) {
-        hour13.addClass('future');
-    } else if (numCurrentHour === 13) {
-        hour13.addClass('present');
-    } else {
-        hour13.addClass('past'); 
-    }
-}
-function pastPresFut_hour14() {
-    if (numCurrentHour < 14) {
-        hour14.addClass('future');
-    } else if (numCurrentHour === 14) {
-        hour14.addClass('present');
-    } else {
-        hour14.addClass('past'); 
-    }
-}
-function pastPresFut_hour15() {
-    if (numCurrentHour < 15) {
-        hour15.addClass('future');
-    } else if (numCurrentHour === 15) {
-        hour15.addClass('present');
-    } else {
-        hour15.addClass('past'); 
-    }
-}
-function pastPresFut_hour16() {
-    if (numCurrentHour < 16) {
-        hour16.addClass('future');
-    } else if (numCurrentHour === 16) {
-        hour16.addClass('present');
-    } else {
-        hour16.addClass('past'); 
-    }
-}
-function pastPresFut_hour17() {
-    if (numCurrentHour < 17) {
-        hour17.addClass('future');
-    } else if (numCurrentHour === 17) {
-        hour17.addClass('present');
-    } else {
-        hour17.addClass('past'); 
-    }
-}
-
-
-
-// 
-
 // pass all jquery arguments here so that the dom loads first
 $(document).ready(function(){
     // puts today's date at the top of the screen using day.js
@@ -124,26 +42,161 @@ $(document).ready(function(){
         localStorage.setItem("event09", JSON.stringify(hour09event.val()));        
     });
 
-    var savedEvent = JSON.parse(localStorage.getItem("event09"));
-    $(hour09event).text(savedEvent);  
+    $(hour10button).on("click", function() {
+        localStorage.setItem("event10", JSON.stringify(hour10event.val()));        
+    });
+
+    $(hour11button).on("click", function() {
+        localStorage.setItem("event11", JSON.stringify(hour11event.val()));        
+    });
+
+    $(hour12button).on("click", function() {
+        localStorage.setItem("event12", JSON.stringify(hour12event.val()));        
+    });
+
+    $(hour13button).on("click", function() {
+        localStorage.setItem("event13", JSON.stringify(hour13event.val()));        
+    });
+
+    $(hour14button).on("click", function() {
+        localStorage.setItem("event14", JSON.stringify(hour14event.val()));        
+    });
+
+    $(hour15button).on("click", function() {
+        localStorage.setItem("event15", JSON.stringify(hour15event.val()));        
+    });
+
+    $(hour16button).on("click", function() {
+        localStorage.setItem("event16", JSON.stringify(hour16event.val()));        
+    });
+
+    $(hour17button).on("click", function() {
+        localStorage.setItem("event17", JSON.stringify(hour17event.val()));        
+    });
+
+    pastPresFut_hour09();
+    pastPresFut_hour10();
+    pastPresFut_hour11();
+    pastPresFut_hour12();
+    pastPresFut_hour13();
+    pastPresFut_hour14();
+    pastPresFut_hour15();
+    pastPresFut_hour16();
+    pastPresFut_hour17();
+
+
+    function pastPresFut_hour09() {
+        if (numCurrentHour < 9) {
+            hour09.addClass('future');
+        } else if (numCurrentHour === 9) {
+            hour09.addClass('present');
+        } else {
+            hour09.addClass('past'); 
+        }
+    }
+
+    function pastPresFut_hour10() {
+            if (numCurrentHour < 10) {
+                hour10.addClass('future');
+            } else if (numCurrentHour === 10) {
+                hour10.addClass('present');
+            } else {
+                hour10.addClass('past'); 
+            }
+    }
+    function pastPresFut_hour11() {
+            if (numCurrentHour < 11) {
+                hour11.addClass('future');
+            } else if (numCurrentHour === 11) {
+                hour11.addClass('present');
+            } else {
+                hour11.addClass('past'); 
+            }
+    }
+    function pastPresFut_hour12() {
+        if (numCurrentHour < 12) {
+            hour12.addClass('future');
+        } else if (numCurrentHour === 12) {
+            hour12.addClass('present');
+        } else {
+            hour12.addClass('past'); 
+        }
+    }
+    function pastPresFut_hour13() {
+        if (numCurrentHour < 13) {
+            hour13.addClass('future');
+        } else if (numCurrentHour === 13) {
+            hour13.addClass('present');
+        } else {
+            hour13.addClass('past'); 
+        }
+    }
+    function pastPresFut_hour14() {
+        if (numCurrentHour < 14) {
+            hour14.addClass('future');
+        } else if (numCurrentHour === 14) {
+            hour14.addClass('present');
+        } else {
+            hour14.addClass('past'); 
+        }
+    }
+    function pastPresFut_hour15() {
+        if (numCurrentHour < 15) {
+            hour15.addClass('future');
+        } else if (numCurrentHour === 15) {
+            hour15.addClass('present');
+        } else {
+            hour15.addClass('past'); 
+        }
+    }
+    function pastPresFut_hour16() {
+        if (numCurrentHour < 16) {
+            hour16.addClass('future');
+        } else if (numCurrentHour === 16) {
+            hour16.addClass('present');
+        } else {
+            hour16.addClass('past'); 
+        }
+    }
+    function pastPresFut_hour17() {
+        if (numCurrentHour < 17) {
+            hour17.addClass('future');
+        } else if (numCurrentHour === 17) {
+            hour17.addClass('present');
+        } else {
+            hour17.addClass('past'); 
+        }
+    }
+
+    var savedEvent09 = JSON.parse(localStorage.getItem("event09"));
+    $(hour09event).text(savedEvent09);  
+
+    var savedEvent10 = JSON.parse(localStorage.getItem("event10"));
+    $(hour10event).text(savedEvent10);  
+
+    var savedEvent11 = JSON.parse(localStorage.getItem("event11"));
+    $(hour11event).text(savedEvent11);  
+
+    var savedEvent12 = JSON.parse(localStorage.getItem("event12"));
+    $(hour12event).text(savedEvent12);  
+
+    var savedEvent13 = JSON.parse(localStorage.getItem("event13"));
+    $(hour13event).text(savedEvent13);  
+
+    var savedEvent14 = JSON.parse(localStorage.getItem("event14"));
+    $(hour14event).text(savedEvent14);  
+
+    var savedEvent15 = JSON.parse(localStorage.getItem("event15"));
+    $(hour15event).text(savedEvent15);  
+
+    var savedEvent16 = JSON.parse(localStorage.getItem("event16"));
+    $(hour16event).text(savedEvent16);  
+
+    var savedEvent17 = JSON.parse(localStorage.getItem("event17"));
+    $(hour17event).text(savedEvent17);  
+
 })
 
 var timeInterval = setInterval(function (){
     // check the time every minute to see which hour we're in
 }, 60000)
-
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?  
